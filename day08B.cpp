b1761212a2e56d65bb5aeb1e84580c8bb0002cc2
aksharye <aksharye@gmail.com>
2024-12-08 00:18:15 -0500

    day 8

---
+++ day08B.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+#define pb push_back
+
+// grid
+vector<string> grid;
+
+// helper to check bounds
+bool check_bounds(int x, int y) {
+    return x >= 0 && x < grid.size() && y >= 0 && y < grid[0].size();
+}
+
+int main() {    
+    // read in grid
+    string line;
+    while (getline(cin, line)) grid.pb(line);
+
+    // keep track of antennas
+    map<char, vector<pair<ll,ll>>> freq;
+    for (int i = 0; i < grid.size(); i++) {
+        for (int j = 0; j < grid[0].size(); j++) {
+            if (grid[i][j] != '.') freq[grid[i][j]].pb({i,j});
+        }
+    }
+
+    // keep track of antinodes
+    set<pair<ll,ll>> antinodes;
+    for (auto [k, v] : freq) {
+        ll n = v.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+                // add antennas as they count
+                antinodes.insert(v[i]);
+                antinodes.insert(v[j]);
+                // keep adding inline antennas until out of bounds
+                ll dx = v[i].first - v[j].first;
+                ll dy = v[i].second - v[j].second;
+                ll x = v[i].first + dx;
+                ll y = v[i].second + dy;
+                while (check_bounds(x,y)) {
+                    antinodes.insert({x,y});
+                    x += dx;
+                    y += dy;
+                }
+            }
+        }
+    }
+
+    cout << antinodes.size() << endl;
+}
